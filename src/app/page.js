b6c68ebec9b779fc1e5c37213cc7ca0c525bc4a9
zainriaz892 <zainbhaisss356@gmail.com>
2024-08-28 17:26:25 +0500

    sorry

---
+++ src/app/page.js
@@ -0,0 +1,32 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Home;
+const image_1 = __importDefault(require("next/image"));
+function Home() {
+    return (<main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <image_1.default className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/ch-pic.jpg" alt="" width="384" height="512"/>
+  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+    </figure>
+    </div>
+    </main>);
+}
